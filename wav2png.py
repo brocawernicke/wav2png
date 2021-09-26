4426ccbf09403842d2c1a114b137d0def117cb96
Broca & Wernicke <sangkyu.ai@gmail.com>
2021-09-26 20:06:25 +0900

    Update wav2png.py

--- wav2png.py
+++ wav2png.py
@@ -9,14 +9,14 @@ w = wave.open("audio.wav", mode="rb")
 frames = w.readframes(w.getnframes())
 data = memoryview(frames).tolist()
 signal_length = w.getnframes()
+S = signal_length/IMAGE_SIZE_X
 
-print(signal_length)
+print(signal_length, S)
 
 im = Image.new("RGB", (IMAGE_SIZE_X,IMAGE_SIZE_Y), (255,255,255))
 draw = ImageDraw.Draw(im)
 
-for py in range(0,IMAGE_SIZE_X,5):
-    S = signal_length/IMAGE_SIZE_X
+for py in range(0,IMAGE_SIZE_X,5):    
     bar = sum(data[int(py*S):int((py+5)*S)])/(2*5*S)
     draw.line((py, IMAGE_SIZE_Y/2+bar, py, IMAGE_SIZE_Y/2-bar), fill=200, width=(BAR_WIDTH-1))
 
